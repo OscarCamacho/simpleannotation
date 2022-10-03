4c4484cc1e28e64ca43f20466e0e203a8f0c986d
Oscar Camacho <oscar.u.camacho.hernandez@gmail.com>
2022-10-02 23:49:33 -0500

    Added accessModifier to AttributeDescripto

--- src/main/java/com/example/simpleannotation/model/descriptors/AttributeDescriptor.java
+++ src/main/java/com/example/simpleannotation/model/descriptors/AttributeDescriptor.java
@@ -2,12 +2,14 @@ package com.example.simpleannotation.model.descriptors;
 
 public final class AttributeDescriptor {
 
+    private final String accessModifier;
     private final String name;
     private final String type;
 
-    public AttributeDescriptor(String name, String type) {
+    public AttributeDescriptor(String name, String type, String accessModifier) {
         this.name = name;
         this.type = type;
+        this.accessModifier = accessModifier;
     }
 
     public String getName() {
@@ -18,8 +20,12 @@ public final class AttributeDescriptor {
         return type;
     }
 
+    public String getAccessModifier () {
+        return this.accessModifier;
+    }
+
     @Override
     public String toString() {
-        return String.join(" ", this.type, this.name);
+        return String.join(" ", this.accessModifier, this.type, this.name);
     }
 }
