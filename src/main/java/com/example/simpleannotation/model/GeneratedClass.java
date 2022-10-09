c89b4e33309a708b96c3e20aa08267daea380177
Oscar U. Camacho Hernandez <oscar.u.camacho.hernandez@gmail.com>
2022-10-08 21:25:32 -0500

    Created GeneratedClass

--- src/main/java/com/example/simpleannotation/model/GeneratedClass.java
+++ src/main/java/com/example/simpleannotation/model/GeneratedClass.java
@@ -2,6 +2,9 @@ package com.example.simpleannotation.model;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Optional;
+
+import com.example.simpleannotation.exceptions.ClassGenerationException;
 import com.example.simpleannotation.model.descriptors.*;
 
 public abstract class GeneratedClass {
@@ -21,9 +24,102 @@ public abstract class GeneratedClass {
         this.methods = new ArrayList<>();
     }
 
+    public Optional<String> getPackageName() {
+        return Optional.ofNullable(packageName);
+    }
+
+    public GeneratedClass setPackageName(String packageName) {
+        this.packageName = packageName;
+        return this;
+    }
+
+    public Optional<String> getClassName() {
+        return Optional.ofNullable(className);
+    }
+
+    public GeneratedClass setClassName(String className) {
+        this.className = className;
+        return this;
+    }
+
+    public Optional<String> getExtendClass() {
+        return Optional.ofNullable(extendClass);
+    }
+
+    public GeneratedClass setExtendClass(String extendClass) {
+        this.extendClass = extendClass;
+        return this;
+    }
+
+    public List<String> getImplementClasses() {
+        return implementClasses;
+    }
+
+    public GeneratedClass setImplementClasses(List<String> implementClasses) {
+        this.implementClasses = implementClasses;
+        return this;
+    }
+
+    public List<AttributeDescriptor> getAttributes() {
+        return attributes;
+    }
+
+    public GeneratedClass setAttributes(List<AttributeDescriptor> attributes) {
+        this.attributes = attributes;
+        return this;
+    }
+
+    public List<ConstructorDescriptor> getConstructors() {
+        return constructors;
+    }
+
+    public GeneratedClass setConstructors(List<ConstructorDescriptor> constructors) {
+        this.constructors = constructors;
+        return this;
+    }
+
+    public List<MethodDescriptor> getMethods() {
+        return methods;
+    }
+
+    public GeneratedClass setMethods(List<MethodDescriptor> methods) {
+        this.methods = methods;
+        return this;
+    }
+
     @Override
-    public String toString() {
-        return "";
+    public final String toString() {
+        StringBuilder classString = new StringBuilder();
+        this.getPackageName().ifPresent(packageNameString -> classString.append("package ")
+                .append(packageNameString).append(";\n"));
+        classString.append("public class ")
+                .append(this.getClassName().orElseThrow(ClassGenerationException::new))
+                .append(" ");
+        this.getExtendClass().ifPresent(extendClassString -> classString.append("extends ")
+                .append(extendClassString).append(" "));
+        if (!this.implementClasses.isEmpty()) {
+            classString.append("implements ");
+            for (int i = 0; i < this.implementClasses.size(); i++) {
+                classString.append(this.implementClasses.get(i));
+                if (i < this.implementClasses.size() - 1) {
+                    classString.append(", ");
+                }
+            }
+        }
+        classString.append("{\n"); // Class begin
+        for (AttributeDescriptor attribute : this.attributes) {
+            classString.append("\t").append(attribute).append(";\n");
+        }
+        for (ConstructorDescriptor constructor : this.constructors) {
+            classString.append("\t").append(constructor).append("\n");
+            // TODO: Figure out constructor body
+        }
+        for (MethodDescriptor method : this.methods) {
+            classString.append("\t").append(method).append("\n");
+            // TODO: Figure out constructor body
+        }
+        classString.append("}\n"); // End of Class
+        return classString.toString();
     }
 
 }
